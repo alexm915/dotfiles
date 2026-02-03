d562529b8791eb8e9e21c9dae706165fae217bba
alex <thealbertmak@gmail.com>
2026-02-03 15:57:28 +0800

    add zshrc devbox alias

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 # ====== basic tools ======
 sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm man-db base-devel curl git zip unzip the_silver_searcher trash-cli tree
-sudo pacman -S --needed --noconfirm zsh neovim tmux fzf ripgrep btop fastfetch lazygit git-delta
+sudo pacman -S --needed --noconfirm zsh neovim tmux fzf ripgrep htop fastfetch lazygit git-delta
 sudo pacman -S --needed --noconfirm cmake gdb clang lldb ninja podman
 sudo pacman -S --needed --noconfirm chezmoi yazi
 
