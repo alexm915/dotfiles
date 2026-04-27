02a152d6b17e518c682c310cdc5b1382efc927fb
alex <thealbertmak@gmail.com>
2026-04-27 11:31:50 +0800

    update install scripts

--- install.sh
+++ install.sh
@@ -14,19 +14,17 @@ sudo pacman -S --needed --noconfirm go lua-jsregexp luarocks
 sudo pacman -S --needed --noconfirm nodejs npm
 sudo npm install -g @anthropic-ai/claude-code
 
-
 # ====== yay ======
 command -v yay >/dev/null || (
-    mkdir -p "${HOME}/tmp" &&
-    cd "${HOME}/tmp" &&
-    rm -rf yay &&
-    git clone https://aur.archlinux.org/yay.git &&
-    cd yay &&
-    makepkg -sic --noconfirm &&
-    cd "${HOME}" &&
-    rm -rf "${HOME}/tmp/yay"
+	mkdir -p "${HOME}/tmp" &&
+		cd "${HOME}/tmp" &&
+		rm -rf yay &&
+		git clone https://aur.archlinux.org/yay.git &&
+		cd yay &&
+		makepkg -sic --noconfirm &&
+		cd "${HOME}" &&
+		rm -rf "${HOME}/tmp/yay"
 )
-
 
 # ====== config yazi ======
 ya pkg add yazi-rs/plugins:full-border
@@ -38,9 +36,8 @@ ya pkg add imsi32/yatline
 
 # ====== config git ======
 git config --global user.name "alex"
-git config --global user.email "thealbertmak@gmail.com"
+git config --global user.email "aliasgmail@duck.com"
 git config --global init.defaultBranch main
-
 
 # ====== config tmux ======
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/
@@ -50,25 +47,10 @@ curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | 
 
 # ====== Set zsh as default shell ======
 if [ "$SHELL" != "$(which zsh)" ]; then
-    echo "Setting zsh as default shell..."
-    chsh -s "$(which zsh)"
-    exec zsh
+	echo "Setting zsh as default shell..."
+	chsh -s "$(which zsh)"
+	exec zsh
 else
-    echo "Your default shell is already zsh."
+	echo "Your default shell is already zsh."
 fi
 
-# ====== Auto Setup Neovim ======
-if command -v nvim &>/dev/null; then
-    echo "Syncing Neovim plugins..."
-    nvim --headless "+Lazy! sync" +qa
-else
-    echo "Neovim not found, skipping plugin sync."
-fi
-
-# ====== initialize dotfiles ======
-if [ ! -d "$HOME/.local/share/chezmoi" ]; then
-    chezmoi init git@github.com:alexm915/dotfiles.git
-    chezmoi apply
-else
-    echo "chezmoi already initialized."
-fi
