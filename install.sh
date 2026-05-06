b98d455db15e66ba0be9d15ff3f3a2ef7413d43a
alex <thealbertmak@gmail.com>
2026-05-06 16:20:41 +0800

    shortcuts changes

--- install.sh
+++ install.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# ====== basic tools ======
 sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm man-db base-devel curl git zip unzip the_silver_searcher trash-cli tree
-sudo pacman -S --needed --noconfirm zsh neovim tmux fzf ripgrep fd htop fastfetch lazygit git-delta
-sudo pacman -S --needed --noconfirm cmake make ninja gdb clang llvm lldb bear podman
-sudo pacman -S --needed --noconfirm chezmoi yazi
-# --- nvim needed pkgs
-cargo install --locked tree-sitter-cli
-sudo pacman -S --needed --noconfirm go lua-jsregexp luarocks
-# --- vibe coding
-sudo pacman -S --needed --noconfirm nodejs npm
-sudo npm install -g @anthropic-ai/claude-code
-
-# ====== yay ======
+# ====== basic tools ======
+sudo pacman -S --needed --noconfirm base-devel man-db curl git zip unzip trash-cli tree
+sudo pacman -S --needed --noconfirm fzf ripgrep fd the_silver_searcher
+sudo pacman -S --needed --noconfirm chezmoi fastfetch
+#yay
 command -v yay >/dev/null || (
 	mkdir -p "${HOME}/tmp" &&
 		cd "${HOME}/tmp" &&
@@ -26,26 +18,36 @@ command -v yay >/dev/null || (
 		rm -rf "${HOME}/tmp/yay"
 )
 
+# ====== dev toolchain ======
+sudo pacman -S --needed --noconfirm htop cmake make ninja gdb clang llvm lldb bear podman
+#claude-code
+sudo pacman -S --needed --noconfirm nodejs npm
+sudo npm install -g @anthropic-ai/claude-code
+
+# ====== config git ======
+sudo pacman -S --needed --noconfirm lazygit git-delta
+git config --global user.name "alex"
+git config --global user.email "aliasgmail@duck.com"
+git config --global init.defaultBranch main
+# ====== config neovim ======
+sudo pacman -S --needed --noconfirm neovim
+cargo install --locked tree-sitter-cli
+sudo pacman -S --needed --noconfirm go lua-jsregexp luarocks
+# ====== config tmux ======
+sudo pacman -S --needed --noconfirm tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # ====== config yazi ======
+sudo pacman -S --needed --noconfirm yazi
 ya pkg add yazi-rs/plugins:full-border
 ya pkg add yazi-rs/plugins:git
 ya pkg add Rolv-Apneseth/starship
 ya pkg add KKV9/compress
 ya pkg add h-hg/yamb
 ya pkg add imsi32/yatline
-
-# ====== config git ======
-git config --global user.name "alex"
-git config --global user.email "aliasgmail@duck.com"
-git config --global init.defaultBranch main
-
-# ====== config tmux ======
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/
-
-# ====== zimfw ======
+# ====== config zsh ======
+sudo pacman -S --needed --noconfirm zsh
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-
-# ====== Set zsh as default shell ======
+#set zsh as default shell
 if [ "$SHELL" != "$(which zsh)" ]; then
 	echo "Setting zsh as default shell..."
 	chsh -s "$(which zsh)"
@@ -53,4 +55,3 @@ if [ "$SHELL" != "$(which zsh)" ]; then
 else
 	echo "Your default shell is already zsh."
 fi
-
